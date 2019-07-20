0c8971164f444ac37bfa7ef8e7a84a233477f438
Fintan Kearney <fintanak@gmail.com>
2019-07-20 21:08:35 +0100

    Development.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,63 @@ import {HttpModule} from '@angular/http';
 import {AppComponent} from './app.component';
 import {OnboardComponent} from './onboard/onboard.component';
 import {RouterModule, Routes} from '@angular/router';
-import { LiveComponent } from './live/live.component';
-import { BalanceComponent } from './balance/balance.component';
-import { MessagesComponent } from './messages/messages.component';
-import { CallsComponent } from './calls/calls.component';
-import { VideoComponent } from './video/video.component';
-import { MusicComponent } from './music/music.component';
-import { DataComponent } from './data/data.component';
-import { SettingsComponent } from './settings/settings.component';
-import { CameraComponent } from './camera/camera.component';
+import {LiveComponent} from './live/live.component';
+import {BalanceComponent} from './balance/balance.component';
+import {MessagesComponent} from './messages/messages.component';
+import {CallsComponent} from './calls/calls.component';
+import {VideoComponent} from './video/video.component';
+import {MusicComponent} from './music/music.component';
+import {DataComponent} from './data/data.component';
+import {SettingsComponent} from './settings/settings.component';
+import {CameraComponent} from './camera/camera.component';
+import {FilesComponent} from './files/files.component';
+import {ChatComponent} from './chat/chat.component';
+import {MoviesComponent} from './movies/movies.component';
+import {ContactsComponent} from './contacts/contacts.component';
+import {PhotosComponent} from './photos/photos.component';
 
 const appRoutes: Routes = [
   { path: 'welcome',
     component: OnboardComponent
+  },
+  { path: 'balance',
+    component: BalanceComponent
+  },
+  { path: 'calls',
+    component: CallsComponent
+  },
+  { path: 'camera',
+    component: CameraComponent
+  },
+  { path: 'chat',
+    component: OnboardComponent
+  },
+  { path: 'contacts',
+    component: ContactsComponent
+  },
+  { path: 'data',
+    component: DataComponent
+  },
+  { path: 'files',
+    component: FilesComponent
+  },
+  { path: 'messages',
+    component: MessagesComponent
+  },
+  { path: 'music',
+    component: MusicComponent
+  },
+  { path: 'settings',
+    component: SettingsComponent
+  },
+  { path: 'movies',
+    component: MoviesComponent
+  },
+  { path: 'video',
+    component: VideoComponent
+  },
+  { path: 'photos',
+    component: PhotosComponent
   },
   {
     path: '',
@@ -44,7 +88,12 @@ const appRoutes: Routes = [
     MusicComponent,
     DataComponent,
     SettingsComponent,
-    CameraComponent
+    CameraComponent,
+    FilesComponent,
+    ChatComponent,
+    MoviesComponent,
+    ContactsComponent,
+    PhotosComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
