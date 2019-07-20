ddf4dd9214eb1937e8a3662a334d96433d465ca2
Fintan Kearney <fintanak@gmail.com>
2019-07-20 20:42:34 +0100

    Development.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,47 @@ import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {AppComponent} from './app.component';
 import {OnboardComponent} from './onboard/onboard.component';
+import {RouterModule, Routes} from '@angular/router';
+import { LiveComponent } from './live/live.component';
+import { BalanceComponent } from './balance/balance.component';
+import { MessagesComponent } from './messages/messages.component';
+import { CallsComponent } from './calls/calls.component';
+import { VideoComponent } from './video/video.component';
+import { MusicComponent } from './music/music.component';
+import { DataComponent } from './data/data.component';
+import { SettingsComponent } from './settings/settings.component';
+import { CameraComponent } from './camera/camera.component';
+
+const appRoutes: Routes = [
+  { path: 'welcome',
+    component: OnboardComponent
+  },
+  {
+    path: '',
+    redirectTo: '/welcome', pathMatch: 'full'
+  }
+];
+
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     AppComponent,
-    OnboardComponent
+    OnboardComponent,
+    LiveComponent,
+    BalanceComponent,
+    MessagesComponent,
+    CallsComponent,
+    VideoComponent,
+    MusicComponent,
+    DataComponent,
+    SettingsComponent,
+    CameraComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
